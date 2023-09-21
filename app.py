0299b209a6cb89defcc788f6a96d4bfad7e4be2c
RK <114596148+Rakesharma319@users.noreply.github.com>
2023-09-21 13:04:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+from Curr_User_Fun import Current_User as CU
 
 st.title('CM DataChat App')
-st.write(st.experimental_user['email'])
-
-st.write(st.experimental_user.email)
+#curr_user=st.write(st.experimental_user['email'])
+curr_user=st.experimental_user['email']
+st.write(CU(curr_user))
