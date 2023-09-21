a331311ff0e975b87c8e5a47a3255d390ca7e2ff
RK <114596148+Rakesharma319@users.noreply.github.com>
2023-09-21 16:29:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def generate_response(input_text):
   )
   st.info(response.choices[0].message["content"])
 
+OutPut_raw=generate_response(Prompt)
 
 # def generate_response(input_text):
 #     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -63,7 +64,7 @@ with st.form('my_form'):
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
     if submitted and openai_api_key.startswith('sk-'):
-      OutPut_raw=generate_response(Prompt)
+      OutPut_raw
       
 
 # Execute SQL in Database.
