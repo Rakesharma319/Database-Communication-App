36b768d45940d704de76f6375eccf3eef462f4df
RK <114596148+Rakesharma319@users.noreply.github.com>
2023-09-21 14:19:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,12 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 from Curr_User_Fun import Current_User as CU
+from GetTableSchema import get_table_schema as gts
 
 st.title('CM DataChat App')
 #curr_user=st.write(st.experimental_user['email'])
 curr_user=st.experimental_user['email']
 st.write(CU(curr_user))
 
-# Connect database
-
-conn = sqlite3.connect('chinook.db')
-c = conn.cursor()
-
-def sq(str,con=conn):
-    return pd.read_sql('''{}'''.format(str), con)
-
-df=sq('''SELECT c.CustomerId, c.FirstName, c.LastName, SUM(i.Total) AS TotalInvoice
-FROM Customer c
-JOIN Invoice i ON c.CustomerId = i.CustomerId
-GROUP BY c.CustomerId, c.FirstName, c.LastName
-ORDER BY TotalInvoice DESC
-LIMIT 5''',conn)
-
-st.write(df)
+st.write(gts)
 
