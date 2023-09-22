a0311cdea2f608515f837a446dbef2a94de540c6
RK <114596148+Rakesharma319@users.noreply.github.com>
2023-09-22 12:43:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,3 +79,5 @@ if UserInput := st.chat_input("Create a Snowflake query for top 5 customers by m
 			message_placeholder.markdown(full_response + "▌")
 		message_placeholder.markdown(full_response)
 	st.session_state.messages.append({"role": "assistant", "content": full_response})
+	
+st.markdown(full_response)
