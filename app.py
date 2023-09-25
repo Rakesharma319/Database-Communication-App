07d3d5d64879e43b2d461b81db1fb460f786be00
RK <114596148+Rakesharma319@users.noreply.github.com>
2023-09-25 16:39:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,9 @@ tables_List = sq(
 
 st.sidebar.text('Tables For Reference')
 # st.sidebar.table(tables_List)
-# df = pd.DataFrame(tables_List, columns=("Sr No.","Table Name")
-st.sidebar.dataframe(tables_List)
+df = pd.DataFrame(tables_List, columns=("Sr No.","Table Name")
+st.sidebar.dataframe(df)
+
 ########### Frame Prompt
 
 dialect="SQL"
