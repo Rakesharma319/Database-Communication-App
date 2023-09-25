4599b587d12902f4d7a4010d1e49289f36bd6016
RK <114596148+Rakesharma319@users.noreply.github.com>
2023-09-25 16:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ tables_List = sq(
 
 st.sidebar.text('Tables For Reference')
 # st.sidebar.table(tables_List)
-df = pd.DataFrame(tables_List, columns=("Sr No.","Table Name")
-st.sidebar.dataframe(df)
+# df = pd.DataFrame(tables_List, columns=("Sr No.","Table Name")
+st.sidebar.dataframe(tables_List, columns=("Sr No.","Table Name")
 
 ########### Frame Prompt
 
