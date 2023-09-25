0abaec43326a58b414769965054f709e42ee6748
RK <114596148+Rakesharma319@users.noreply.github.com>
2023-09-25 16:00:34 +0530

    Update Curr_User_Fun.py

--- Curr_User_Fun.py
+++ Curr_User_Fun.py
@@ -1,3 +1,13 @@
-def Current_User(Email_id):
-  d2=Email_id.split("@",1)
-  return "Hello "+ d2[0]
+# def Current_User(Email_id):
+#   d2=Email_id.split("@",1)
+#   return "Hello "+ d2[0]
+
+
+def Current_User(string,delimiters):
+  for delimiter in delimiters:
+     string = " ".join(string.split(delimiter))
+ 
+  result = string.split()
+  result=result[0]
+ 
+  print(result)
